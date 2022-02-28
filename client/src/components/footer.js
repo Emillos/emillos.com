30f8c1e3e1ddcd266673e2d56f16a5cc63588748
Emil Filtenborg Henriksen <emilhenriksen@yahoo.dk>
2022-03-01 00:55:28 +0100

    changed the footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -4,38 +4,19 @@ import { useLocation } from 'react-router-dom'
 import footerData from '../../footerData.json'
 
 const Footer = () => {
-  const [localFooterData, setLocalFooterData] = useState()
-  const location = useLocation();
-  useEffect(() => {
-    setLocalFooterData(footerData[location.pathname])    
-  })
   return (
     <div id='footer'>
-      {localFooterData &&
-        <div>
-          <div id='footerTitles'>
-          <div id="source">{`${localFooterData.name} Project source`}</div>
-          <div id="madeWith">Made with</div>
-        </div>
-
-        <div id='projectDetails'>
-          <div id='footerGithubLink'>
-            <a href={localFooterData.source.url} target={localFooterData.source.target}>
-              <img src={localFooterData.source.image}  alt={localFooterData.source.alt} />
-            </a>
-          </div>
-          <div id='footerTechList'>
-            {localFooterData.techList.map(item => {
-              return( 
-                <a href={item.url} target={item.target} key={item.alt}>
-                  <img src={item.image} alt={item.alt}/>
-                </a>
-              )
-            })}
-          </div>
-        </div>
-      </div>
-    }
+        <h2 className='footerItem'>
+          <a href='https://github.com/Emillos/emillos.com' target='_blank'>
+            <img src='https://client-images-emillos.s3.eu-west-1.amazonaws.com/GitHub-64.png'/>
+          </a>
+        </h2>
+        <h2 className='footerItem'></h2>
+        <h2 className='footerItem'>
+          <a href='https://www.linkedin.com/in/emil-filtenborg-99551699/' target='_blank'>
+            <img src='https://cdn-icons-png.flaticon.com/512/174/174857.png'/>
+          </a>
+        </h2>
     </div>
   )
 }
